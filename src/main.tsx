74eb828be5f5d14d1d45682fb0c91e8c07ee8de9
johnbosco35 <okonkwovincent63@gmail.com>
2025-02-14 17:09:18 +0100

    added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+import store, { persistor } from "./components/redux/store.ts";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { RouterProvider } from 'react-router-dom'
+import { mainRouter } from './router/mainRouter.tsx'
+import { DarkModeProvider } from "./components/context/DarkModeContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+    <ToastContainer position="bottom-right" autoClose={5000} />
+    <DarkModeProvider>
+      <RouterProvider router={mainRouter} />
+      </DarkModeProvider>
+    </PersistGate>
+
+  </Provider>
 )
